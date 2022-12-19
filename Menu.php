66ad350406608d3dbd22ff908639bf84783de409
NikolayIssa <120794043+NikolayIssa@users.noreply.github.com>
2022-12-19 23:38:16 +0300

    Update Menu.php

--- Menu.php
+++ Menu.php
@@ -22,28 +22,28 @@
                     <hr>
                     <div class="menulist">
                                 <div class="menuitem">
-                                    <h1>Пицца <br> "Бебра"</h1>
+                                    <h1>Пицца <br> "Бубульгум"</h1>
                                     <img src="img/assortiment/eTqLi_KCNNky3WrCqbDeOqmpzrQ274xSd-eLnUxtugn8nwENc2hNAoZTEZFlRkW8pGeaLT1DdxHSd4gN1K0CiSM.jpg">
                                     <p>470р</p>
                                     <button class="button-31">Заказать</button>
                                 </div>
 
                                 <div class="menuitem">
-                                    <h1>Пицца <br> "Какич"</h1>
+                                    <h1>Пицца <br> "Картошка фри"</h1>
                                     <img src="img/assortiment/hakw4Iyh6wHCV1Sz3dmoH6Js_2lSXZmxK9VyaCuz-2WpjeI_Ne08PGxR-nuNzLHAgnzMo5bYTjzc6z2h78zHcgM.jpg">
                                     <p>470р</p>
                                     <button class="button-31">Заказать</button>
                                 </div>
 
                                 <div class="menuitem">
-                                    <h1>Пицца <br> "Безумие"</h1>
+                                    <h1>Пицца <br> "Ананасовая"</h1>
                                     <img src="img/assortiment/IeqHqkF9i9ZZ9OWxmYu0YBGt2C47K1jBP2TW2_eD45Wt-nhPX1zogIuEYJKh5-jWW3f2V694x9MXulNFfqzIo04.jpg">
                                     <p>470р</p>
                                     <button class="button-31">Заказать</button>
                                 </div>
 
                                 <div class="menuitem">
-                                    <h1>Пицца <br> "Жоподробилка"</h1>
+                                    <h1>Пицца <br> "С колбасой"</h1>
                                     <img src="img/assortiment/Jy5N8B23KUTwkmdv_9sAXDfsBU-4jp6_5JqWpVezKzSphREx6YwQYp8mr8EZ7k5RcGiWNR0x633UPkAG3OCYzL0.jpg">
                                     <p>470р</p>
                                     <button class="button-31">Заказать</button>
@@ -57,7 +57,7 @@
                                 </div>
 
                                 <div class="menuitem">
-                                    <h1>Пицца <br> "Перегар"</h1>
+                                    <h1>Пицца <br> "Сцепление"</h1>
                                     <img src="img/assortiment/Q3Ul5B9GcYn_3puS7GjcwKzXp8nxHPHhVfthBth4u1VqzSM3LTWGwpIqshNbZShNKKwIrwS6izYYVQaV51ZWgbY.jpg">
                                     <p>470р</p>
                                     <button class="button-31">Заказать</button>
